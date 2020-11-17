fa77f588a957e8cc7b0a2145f33a13f0bc0d35f9
Ilya Sirosh <sirosh.ilya@gmail.com>
2020-11-17 16:56:24 +0200

    Initial Commit

---
+++ Calculator_SwiftUI/Calculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_SwiftUIApp.swift
+//  Calculator_SwiftUI
+//
+//  Created by Illya Sirosh on 17.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
